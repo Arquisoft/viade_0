9b7df7f43148aca237051732def3ca76e5fefa9b
Jose Emilio Labra Gayo <jelabra@gmail.com>
2020-03-02 14:26:59 +0100

    Pruebas fallan

--- src/App.test.js
+++ src/App.test.js
@@ -6,8 +6,8 @@ test('add 2+2', () => {
   expect(2+2).toBe(4);
 }); 
 
-/*test('renders learn react link', () => {
+test('renders learn react link', () => {
   const { getByText } = render(<App />);
   const linkElement = getByText(/Lucía/i);
   expect(linkElement).toBeInTheDocument();
-}); */
+}); 
