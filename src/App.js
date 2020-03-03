7ed0cef612429d8762fd3a1686381f070507b7c4
Jose Emilio Labra Gayo <jelabra@gmail.com>
2020-03-03 10:24:23 +0100

    Changed Ana by Mario

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Welcome name="Ana"/>
+        <Welcome name="Mario"/>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
