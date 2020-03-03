1f2730c30c9c4e92aa3ada8e1358b2bdbf399cde
Jose Emilio Labra Gayo <jelabra@gmail.com>
2020-03-03 10:31:21 +0100

    Commit que falla

--- src/App.test.js
+++ src/App.test.js
@@ -6,8 +6,8 @@ test('add 2+2', () => {
   expect(2+2).toBe(4);
 }); 
 
-/*test('renders learn react link', () => {
+test('renders learn react link', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/Lucía/i);
+  const linkElement = getByText(/Pepe/i);
   expect(linkElement).toBeInTheDocument();
-}); */ 
+}); 
